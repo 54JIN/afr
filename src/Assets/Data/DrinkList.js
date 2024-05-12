7691cbd9276ae9882cf77df8c2ee7233e770c23e
Sajin Saju <sajinsaju2002@gmail.com>
2024-05-12 06:14:51 -0400

    Updated Review component to take in props

--- src/Assets/Data/DrinkList.js
+++ src/Assets/Data/DrinkList.js
@@ -6,19 +6,55 @@ import gongCha from '../Images/gongCha.jpg';
 const DrinkList = [
     {
         image: miTea,
-        title: "MI'N TEA"
+        title: "Mi Tea",
+        address: "1784 Lincoln Hwy, Edison, NJ, 08817, US",
+        phone: "+1 (732) 253-0351",
+        cuisine: "Bubble Tea",
+        critique: [
+            "Nestled in a cozy corner, the restaurant offers an ambiance that is nothing short of enchanting, making it an ideal locale for both romantic evenings and sizable gatherings. Upon entering, one is immediately struck by the meticulous cleanliness and the prompt, attentive service that sets the stage for a delightful dining experience.",
+            "The culinary offerings arrive piping hot, their presentation a feast for the eyes, promising flavors as compelling as their visuals. The filet mignon pho, in particular, strikes a nostalgic chord, its rich, savory broth perfectly complementing the tender slices of beef. Meanwhile, the grilled pork spring rolls are a revelation—crisp, flavorful, and utterly irresistible, especially when paired with a velvety peanut sauce that adds just the right touch of sweetness and depth.",
+            "The Vietnamese coffee, however, while promising, falls slightly short of its potential. The flavor profile begs for refinement to reach the same heights of excellence as the rest of the meal.",
+            "In terms of value, the pricing is well-matched to the quality of food and service provided, making each dish a worthwhile indulgence. This restaurant is unquestionably deserving of a return visit, offering a dining experience that tantalizingly hints at becoming a staple for those in pursuit of culinary delights."
+        ]
     },
     {
         image: utepia,
-        title: "Utepia"
+        title: "Utepia",
+        address: "48 Bayard St, New Brunswick, NJ, 08901, US",
+        phone: "+1 (732) 253-7517",
+        cuisine: "Bubble Tea",
+        critique: [
+            "Nestled in a cozy corner, the restaurant offers an ambiance that is nothing short of enchanting, making it an ideal locale for both romantic evenings and sizable gatherings. Upon entering, one is immediately struck by the meticulous cleanliness and the prompt, attentive service that sets the stage for a delightful dining experience.",
+            "The culinary offerings arrive piping hot, their presentation a feast for the eyes, promising flavors as compelling as their visuals. The filet mignon pho, in particular, strikes a nostalgic chord, its rich, savory broth perfectly complementing the tender slices of beef. Meanwhile, the grilled pork spring rolls are a revelation—crisp, flavorful, and utterly irresistible, especially when paired with a velvety peanut sauce that adds just the right touch of sweetness and depth.",
+            "The Vietnamese coffee, however, while promising, falls slightly short of its potential. The flavor profile begs for refinement to reach the same heights of excellence as the rest of the meal.",
+            "In terms of value, the pricing is well-matched to the quality of food and service provided, making each dish a worthwhile indulgence. This restaurant is unquestionably deserving of a return visit, offering a dining experience that tantalizingly hints at becoming a staple for those in pursuit of culinary delights."
+        ]
     },
     {
         image: cafeZIO,
-        title: "Cafe ZIO"
+        title: "Cafe ZIO",
+        address: "352 Ryders Ln, East Brunswick, NJ, 08816, US",
+        phone: "+1 (732) 339-8138",
+        cuisine: "Bubble Tea",
+        critique: [
+            "Nestled in a cozy corner, the restaurant offers an ambiance that is nothing short of enchanting, making it an ideal locale for both romantic evenings and sizable gatherings. Upon entering, one is immediately struck by the meticulous cleanliness and the prompt, attentive service that sets the stage for a delightful dining experience.",
+            "The culinary offerings arrive piping hot, their presentation a feast for the eyes, promising flavors as compelling as their visuals. The filet mignon pho, in particular, strikes a nostalgic chord, its rich, savory broth perfectly complementing the tender slices of beef. Meanwhile, the grilled pork spring rolls are a revelation—crisp, flavorful, and utterly irresistible, especially when paired with a velvety peanut sauce that adds just the right touch of sweetness and depth.",
+            "The Vietnamese coffee, however, while promising, falls slightly short of its potential. The flavor profile begs for refinement to reach the same heights of excellence as the rest of the meal.",
+            "In terms of value, the pricing is well-matched to the quality of food and service provided, making each dish a worthwhile indulgence. This restaurant is unquestionably deserving of a return visit, offering a dining experience that tantalizingly hints at becoming a staple for those in pursuit of culinary delights."
+        ]
     },
     {
         image: gongCha,
-        title: "Gong Cha"
+        title: "Gong Cha",
+        address: "396 George St, New Brunswick, NJ, 08901, US",
+        phone: "+1 (732) 253-7662",
+        cuisine: "Bubble Tea",
+        critique: [
+            "Nestled in a cozy corner, the restaurant offers an ambiance that is nothing short of enchanting, making it an ideal locale for both romantic evenings and sizable gatherings. Upon entering, one is immediately struck by the meticulous cleanliness and the prompt, attentive service that sets the stage for a delightful dining experience.",
+            "The culinary offerings arrive piping hot, their presentation a feast for the eyes, promising flavors as compelling as their visuals. The filet mignon pho, in particular, strikes a nostalgic chord, its rich, savory broth perfectly complementing the tender slices of beef. Meanwhile, the grilled pork spring rolls are a revelation—crisp, flavorful, and utterly irresistible, especially when paired with a velvety peanut sauce that adds just the right touch of sweetness and depth.",
+            "The Vietnamese coffee, however, while promising, falls slightly short of its potential. The flavor profile begs for refinement to reach the same heights of excellence as the rest of the meal.",
+            "In terms of value, the pricing is well-matched to the quality of food and service provided, making each dish a worthwhile indulgence. This restaurant is unquestionably deserving of a return visit, offering a dining experience that tantalizingly hints at becoming a staple for those in pursuit of culinary delights."
+        ]
     },
 ];
 
--- src/Assets/Data/FoodList.js
+++ src/Assets/Data/FoodList.js
@@ -10,41 +10,108 @@ import mariasBrunch from '../Images/mariasBrunch.jpg';
 const FoodList = [
     {
         image: phoToday,
-        title: "Pho Today"
+        title: "Pho Today",
+        address: "101 New World Way, South Plainfield, NJ, 07080, US",
+        phone: "+1 (908) 822-2030",
+        cuisine: "Vietnamese",
+        critique: [
+            "Nestled in a cozy corner, the restaurant offers an ambiance that is nothing short of enchanting, making it an ideal locale for both romantic evenings and sizable gatherings. Upon entering, one is immediately struck by the meticulous cleanliness and the prompt, attentive service that sets the stage for a delightful dining experience.",
+            "The culinary offerings arrive piping hot, their presentation a feast for the eyes, promising flavors as compelling as their visuals. The filet mignon pho, in particular, strikes a nostalgic chord, its rich, savory broth perfectly complementing the tender slices of beef. Meanwhile, the grilled pork spring rolls are a revelation—crisp, flavorful, and utterly irresistible, especially when paired with a velvety peanut sauce that adds just the right touch of sweetness and depth.",
+            "The Vietnamese coffee, however, while promising, falls slightly short of its potential. The flavor profile begs for refinement to reach the same heights of excellence as the rest of the meal.",
+            "In terms of value, the pricing is well-matched to the quality of food and service provided, making each dish a worthwhile indulgence. This restaurant is unquestionably deserving of a return visit, offering a dining experience that tantalizingly hints at becoming a staple for those in pursuit of culinary delights."
+        ]
     },
     {
         image: happyLamb,
-        title: "Happy Lamb Hot Pot"
+        title: "Happy Lamb Hot Pot",
+        address: "1737 Rte 27, Edison, NJ, 08817, US",
+        phone: "+1 (732) 626-5991",
+        cuisine: "Hot Pot",
+        critique: [
+            "The culinary offerings arrive piping hot, their presentation a feast for the eyes, promising flavors as compelling as their visuals. The filet mignon pho, in particular, strikes a nostalgic chord, its rich, savory broth perfectly complementing the tender slices of beef. Meanwhile, the grilled pork spring rolls are a revelation—crisp, flavorful, and utterly irresistible, especially when paired with a velvety peanut sauce that adds just the right touch of sweetness and depth.",
+            "Nestled in a cozy corner, the restaurant offers an ambiance that is nothing short of enchanting, making it an ideal locale for both romantic evenings and sizable gatherings. Upon entering, one is immediately struck by the meticulous cleanliness and the prompt, attentive service that sets the stage for a delightful dining experience.",
+            "The Vietnamese coffee, however, while promising, falls slightly short of its potential. The flavor profile begs for refinement to reach the same heights of excellence as the rest of the meal.",
+            "In terms of value, the pricing is well-matched to the quality of food and service provided, making each dish a worthwhile indulgence. This restaurant is unquestionably deserving of a return visit, offering a dining experience that tantalizingly hints at becoming a staple for those in pursuit of culinary delights."
+        ]
     },
     {
         image: ramenNagomi,
-        title: "Ramen Nagomi"
+        title: "Ramen Nagomi",
+        address: "49 Bayard St, New Brunswick, NJ, 08901, US",
+        phone: "+1 (732) 317-2623",
+        cuisine: "Ramen",
+        critique: [
+            "Nestled in a cozy corner, the restaurant offers an ambiance that is nothing short of enchanting, making it an ideal locale for both romantic evenings and sizable gatherings. Upon entering, one is immediately struck by the meticulous cleanliness and the prompt, attentive service that sets the stage for a delightful dining experience.",
+            "The culinary offerings arrive piping hot, their presentation a feast for the eyes, promising flavors as compelling as their visuals. The filet mignon pho, in particular, strikes a nostalgic chord, its rich, savory broth perfectly complementing the tender slices of beef. Meanwhile, the grilled pork spring rolls are a revelation—crisp, flavorful, and utterly irresistible, especially when paired with a velvety peanut sauce that adds just the right touch of sweetness and depth.",
+            "The Vietnamese coffee, however, while promising, falls slightly short of its potential. The flavor profile begs for refinement to reach the same heights of excellence as the rest of the meal.",
+            "In terms of value, the pricing is well-matched to the quality of food and service provided, making each dish a worthwhile indulgence. This restaurant is unquestionably deserving of a return visit, offering a dining experience that tantalizingly hints at becoming a staple for those in pursuit of culinary delights."
+        ]
     },
     {
         image: tofuHouse,
-        title: "Tofu House"
+        title: "Tofu House",
+        address: "1755 NJ-27, Edison, NJ, 08817, US",
+        phone: "+1 (732) 777-2110",
+        cuisine: "Korean",
+        critique: [
+            "Nestled in a cozy corner, the restaurant offers an ambiance that is nothing short of enchanting, making it an ideal locale for both romantic evenings and sizable gatherings. Upon entering, one is immediately struck by the meticulous cleanliness and the prompt, attentive service that sets the stage for a delightful dining experience.",
+            "The culinary offerings arrive piping hot, their presentation a feast for the eyes, promising flavors as compelling as their visuals. The filet mignon pho, in particular, strikes a nostalgic chord, its rich, savory broth perfectly complementing the tender slices of beef. Meanwhile, the grilled pork spring rolls are a revelation—crisp, flavorful, and utterly irresistible, especially when paired with a velvety peanut sauce that adds just the right touch of sweetness and depth.",
+            "The Vietnamese coffee, however, while promising, falls slightly short of its potential. The flavor profile begs for refinement to reach the same heights of excellence as the rest of the meal.",
+            "In terms of value, the pricing is well-matched to the quality of food and service provided, making each dish a worthwhile indulgence. This restaurant is unquestionably deserving of a return visit, offering a dining experience that tantalizingly hints at becoming a staple for those in pursuit of culinary delights."
+        ]
     },
     {
         image: sakana,
-        title: "Sakana"
+        title: "Sakana",
+        address: "338 George St, New Brunswick, NJ, 08901, US",
+        phone: "+1 (732) 867-9308",
+        cuisine: "Sushi",
+        critique: [
+            "Nestled in a cozy corner, the restaurant offers an ambiance that is nothing short of enchanting, making it an ideal locale for both romantic evenings and sizable gatherings. Upon entering, one is immediately struck by the meticulous cleanliness and the prompt, attentive service that sets the stage for a delightful dining experience.",
+            "The culinary offerings arrive piping hot, their presentation a feast for the eyes, promising flavors as compelling as their visuals. The filet mignon pho, in particular, strikes a nostalgic chord, its rich, savory broth perfectly complementing the tender slices of beef. Meanwhile, the grilled pork spring rolls are a revelation—crisp, flavorful, and utterly irresistible, especially when paired with a velvety peanut sauce that adds just the right touch of sweetness and depth.",
+            "The Vietnamese coffee, however, while promising, falls slightly short of its potential. The flavor profile begs for refinement to reach the same heights of excellence as the rest of the meal.",
+            "In terms of value, the pricing is well-matched to the quality of food and service provided, making each dish a worthwhile indulgence. This restaurant is unquestionably deserving of a return visit, offering a dining experience that tantalizingly hints at becoming a staple for those in pursuit of culinary delights."
+        ]
     },
     {
         image: laCatrina,
-        title: "La Catrina"
+        title: "La Catrina",
+        address: "144 Easton Ave, New Brunswick, NJ, 08901, US",
+        phone: "+1 (732) 628-0451",
+        cuisine: "Taco",
+        critique: [
+            "Nestled in a cozy corner, the restaurant offers an ambiance that is nothing short of enchanting, making it an ideal locale for both romantic evenings and sizable gatherings. Upon entering, one is immediately struck by the meticulous cleanliness and the prompt, attentive service that sets the stage for a delightful dining experience.",
+            "The culinary offerings arrive piping hot, their presentation a feast for the eyes, promising flavors as compelling as their visuals. The filet mignon pho, in particular, strikes a nostalgic chord, its rich, savory broth perfectly complementing the tender slices of beef. Meanwhile, the grilled pork spring rolls are a revelation—crisp, flavorful, and utterly irresistible, especially when paired with a velvety peanut sauce that adds just the right touch of sweetness and depth.",
+            "The Vietnamese coffee, however, while promising, falls slightly short of its potential. The flavor profile begs for refinement to reach the same heights of excellence as the rest of the meal.",
+            "In terms of value, the pricing is well-matched to the quality of food and service provided, making each dish a worthwhile indulgence. This restaurant is unquestionably deserving of a return visit, offering a dining experience that tantalizingly hints at becoming a staple for those in pursuit of culinary delights."
+        ]
     },
     {
         image: cuzins,
-        title: "Cuzins"
+        title: "Cuzins",
+        address: "78 Albany St, New Brunswick, NJ, 08901, US",
+        phone: "+1 (732) 979-2141",
+        cuisine: "Seafood",
+        critique: [
+            "Nestled in a cozy corner, the restaurant offers an ambiance that is nothing short of enchanting, making it an ideal locale for both romantic evenings and sizable gatherings. Upon entering, one is immediately struck by the meticulous cleanliness and the prompt, attentive service that sets the stage for a delightful dining experience.",
+            "The culinary offerings arrive piping hot, their presentation a feast for the eyes, promising flavors as compelling as their visuals. The filet mignon pho, in particular, strikes a nostalgic chord, its rich, savory broth perfectly complementing the tender slices of beef. Meanwhile, the grilled pork spring rolls are a revelation—crisp, flavorful, and utterly irresistible, especially when paired with a velvety peanut sauce that adds just the right touch of sweetness and depth.",
+            "The Vietnamese coffee, however, while promising, falls slightly short of its potential. The flavor profile begs for refinement to reach the same heights of excellence as the rest of the meal.",
+            "In terms of value, the pricing is well-matched to the quality of food and service provided, making each dish a worthwhile indulgence. This restaurant is unquestionably deserving of a return visit, offering a dining experience that tantalizingly hints at becoming a staple for those in pursuit of culinary delights."
+        ]
     },
     {
         image: mariasBrunch,
-        title: "Marias Brunch"
+        title: "Marias Brunch",
+        address: "140 Durham Ave, Metuchen, NJ, 08840, US",
+        phone: "+1 (732) 243-9050",
+        cuisine: "Breakfast and Brunch",
+        critique: [
+            "Nestled in a cozy corner, the restaurant offers an ambiance that is nothing short of enchanting, making it an ideal locale for both romantic evenings and sizable gatherings. Upon entering, one is immediately struck by the meticulous cleanliness and the prompt, attentive service that sets the stage for a delightful dining experience.",
+            "The culinary offerings arrive piping hot, their presentation a feast for the eyes, promising flavors as compelling as their visuals. The filet mignon pho, in particular, strikes a nostalgic chord, its rich, savory broth perfectly complementing the tender slices of beef. Meanwhile, the grilled pork spring rolls are a revelation—crisp, flavorful, and utterly irresistible, especially when paired with a velvety peanut sauce that adds just the right touch of sweetness and depth.",
+            "The Vietnamese coffee, however, while promising, falls slightly short of its potential. The flavor profile begs for refinement to reach the same heights of excellence as the rest of the meal.",
+            "In terms of value, the pricing is well-matched to the quality of food and service provided, making each dish a worthwhile indulgence. This restaurant is unquestionably deserving of a return visit, offering a dining experience that tantalizingly hints at becoming a staple for those in pursuit of culinary delights."
+        ]
     },
-    {
-        image: mariasBrunch,
-        title: "Marias Brunch"
-    },
-    
 ];
 
 export default FoodList;
